5675e091be343870404179156f8d6ca7ef7a42a4
machty <machty@gmail.com>
2016-02-29 07:04:16 -0500

    remove trololol slide
    what are you, 12?

--- app/application/controller.js
+++ app/application/controller.js
@@ -44,7 +44,7 @@ export default Ember.Controller.extend(Ember.Evented, {
     "what-about-concurrency-5",
     "what-about-concurrency-6",
     "task-modifiers-1",
-    "task-modifiers-2",
+    //"task-modifiers-2",
     "task-modifiers-3",
     "task-modifiers-4",
     "task-modifiers-5",
